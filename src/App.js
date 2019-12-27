7dd4bc2870646452f554c7ca041ba91f4d5a87fc
João Paulo <lopes_jpaulo@hotmail.com>
2019-12-27 13:59:18 -0300

    Added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { React } from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
 import Routes from './routes';
 
 export default function App() {
-    return(
+    return (
         <BrowserRouter>
+            <Header/>
             <Routes />
+            <GlobalStyle />
         </BrowserRouter>
-    )
+    );
 }
